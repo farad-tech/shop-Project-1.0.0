bcdb9206a1a032aaad02ea609fa9f75dc405a682
Farhad Karami <farhadkarami@yahoo.com>
2022-03-22 20:39:45 +0430

    add first divation of header of the page

---
+++ Components/headerNav.php
@@ -0,0 +1,6 @@
+<header class="px-1">
+    <div class="d-flex justify-content-between py-2">
+        <img class="img-fluid" src="#" alt="Web Logo">
+        <span class="text-muted">2022/ 02/ 22 <i class="bi bi-calendar"></i></span>
+    </div>
+</header>
