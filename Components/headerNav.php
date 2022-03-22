daa337f2c4cab0dcaffa5d8e18f71b2b8468d6ad
Farhad Karami <farhadkarami@yahoo.com>
2022-03-23 00:32:15 +0430

    Header of the site complete with the writing third section of the divation and some related css style

--- Components/headerNav.php
+++ Components/headerNav.php
@@ -1,18 +1,25 @@
-<header class="px-2">
-    <div class="d-flex justify-content-between py-2">
+<header class="px-2 bg-light shadow-sm">
+    <div class="d-flex justify-content-between py-2 border-bottom">
         <img class="img-fluid" src="#" alt="Auto Parts Website">
         <span class="nav-date"> <?php echo date('Y/m/d D'); ?> <i class="bi bi-calendar"></i></span>
     </div>
-    <nav class="navbar navbar-expand-lg">
+
+    <nav class="navbar navbar-expand-lg border-bottom small">
         <div class="container-fluid">
-            <a class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-toggler p-0" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="bi bi-list"></i>
             </a>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <ul class="navbar-nav m-auto">
+                <ul class="navbar-nav m-auto d-flex">
                     <?php require_once "headerNavComponents/navbarNav.php"; ?>
                 </ul>
             </div>
         </div>
     </nav>
+
+    <div class="d-flex flex-row-reverse flex-wrap py-2">
+        <a class="btn-primary nav-link px-1" href="#" rel="noopener noreferrer">Account <i class="bi bi-person-fill"></i></a>
+        <a class="nav-link" href="#" rel="noopener noreferrer"><i class="bi bi-cart"></i></a>
+        <a class="nav-link" href="#" rel="noopener noreferrer">Iran, Tehran, Azadi ... <i class="bi bi-geo-alt-fill"></i></a>
+    </div>
 </header>
