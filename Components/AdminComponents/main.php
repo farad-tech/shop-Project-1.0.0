637e47196af83c675508e5466026f6e68e6caf75
Farhad Karami <farhadkarami@yahoo.com>
2022-03-23 23:00:48 +0430

    Front of adding crousel images form created

--- Components/AdminComponents/main.php
+++ Components/AdminComponents/main.php
@@ -1,3 +1,12 @@
-<main>
-    
+<main class="container ">
+    <form action="" method="post">
+        <div class="d-flex flex-column mt-4">
+            <h3>Add Carousel images</h3>
+            <label for="#imageLinkID">Image Link</label>
+            <input class="form-control" type="text" name="imageLink" id="imageLinkID" placeholder="Enter the image link">
+            <label for="#imageLinkID">Title</label>
+            <input class="form-control" type="text" name="imageTitle" id="imageTitleID" placeholder="Enter the title of the images">
+            <button class="btn my-4 btn-submit" type="submit">Submit</button>
+        </div>
+    </form>
 </main>
