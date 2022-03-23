53eeb922c2368e6cef30517381488c1612ca77fa
Farhad Karami <farhadkarami@yahoo.com>
2022-03-23 22:10:02 +0430

    the Base of Admin page created

---
+++ admin.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+require_once "Components/HTMLHead.php";
+require_once "Components/CallJS.php";
+
+echo "<!DOCTYPE html>";
+echo "<html lang='en'>";
+
+HTMLhead('Online Shop Website', 'CSS/Style.css');
+
+echo "<body>";
+
+require_once "Components/headerNav.php";
+JavaScript("Javascript/Bootstrap/bootstrap.bundle.min.js");
+require_once "Components/AdminComponents/main.php";
+
+echo "</body>";
+echo "</html>";
